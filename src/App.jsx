1a27e244ec4d5130f4f22ef318fda6c8cd1815ea
tijothomas2000 <tijonarithookil@gmail.com>
2024-10-13 11:57:52 +0530

    Folder structure created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      FO SOFT FINAL PROJECT.
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            Navbar
+        </nav>
+    )
+}
+
+export default Navbar;
