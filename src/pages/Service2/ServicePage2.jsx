251df94b03ceb3e7453e09f359d152272d6aa71c
tijothomas2000 <tijonarithookil@gmail.com>
2024-10-13 12:07:16 +0530

    Pages structure created

---
+++ src/pages/Service2/ServicePage2.jsx
@@ -0,0 +1,11 @@
+import './ServicePage2.scss';
+
+const ServicePage2 = () => {
+    return (
+        <div className='servicepage2'>
+            ServicePage2
+        </div>
+    )
+}
+
+export default ServicePage2
